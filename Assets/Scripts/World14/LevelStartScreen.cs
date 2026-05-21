70871fcb2585688209f645fef8d03cd910743135
Mirkof13 <miguel13.mirkof@gmail.com>
2026-05-21 06:12:21 -0400

    Initial commit - Super Mario project by mirkof

---
+++ Assets/Scripts/World14/LevelStartScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+
+
+public class LevelStartScreen : MonoBehaviour {
+	private GameStateManager t_GameStateManager;
+	private float loadScreenDelay = 2;
+
+	public Text WorldTextHUD;
+	public Text ScoreTextHUD;
+	public Text CoinTextHUD;
+	public Text WorldTextMain;
+	public Text livesText;
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1;
+
+		t_GameStateManager = FindObjectOfType<GameStateManager> ();
+		string worldName = t_GameStateManager.sceneToLoad;
+		if (worldName == null) worldName = "1-1";
+
+		// Handle both "World 1-4" (original) and "1-4" (merged project) naming
+		string displayName = worldName.Contains("World ") ? Regex.Split (worldName, "World ")[1] : worldName;
+		WorldTextHUD.text = displayName;
+		ScoreTextHUD.text = t_GameStateManager.scores.ToString ("D6");
+		CoinTextHUD.text = "x" + t_GameStateManager.coins.ToString ("D2");
+		WorldTextMain.text = ("WORLD " + displayName).ToUpper ();
+		livesText.text = t_GameStateManager.lives.ToString ();
+
+		StartCoroutine (LoadSceneDelayCo (t_GameStateManager.sceneToLoad, loadScreenDelay));
+
+		Debug.Log (this.name + " Start: current scene is " + SceneManager.GetActiveScene ().name);
+	}
+	
+	IEnumerator LoadSceneDelayCo(string sceneName, float delay) {
+		yield return new WaitForSecondsRealtime (delay);
+		SceneManager.LoadScene (sceneName);
+	}
+}
